2582de4c171c7d74f1e79c686ea9848d0014a1dc
Brandon Arensmeyer <b73x527@gfcmsu.edu>
2023-02-28 19:12:59 -0700

    header

--- CafeBrandon/logIn.php
+++ CafeBrandon/logIn.php
@@ -5,10 +5,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/logIn.css">
+    <link rel="stylesheet" href="css/logIn.css">
     <title>Log In</title>
 </head>
 <body>
-    <button id="home" type="submit"> Home </button>
+    <header>
+        <?php
+            include 'header/header.php';
+        ?>
+        <div id="top">
+            <form id="head" action="signIn.php">
+               <button id="header" type="submit" > Sign In </button>
+             </form>        
+             <form id="head" action="logIn.php">
+                <button id="header" type="submit" > Log In </button>
+             </form>        
+             <form id="head" action="order.php">
+                <button id="header" type="submit" > Order </button>
+             </form>  
+        </div>      
+    </header>
     <main>
         <?php if ($_SERVER['REQUEST_METHOD'] === 'GET') : ?>
             <?php echo "<h2>Please enter your account information below</h2>"; ?>
