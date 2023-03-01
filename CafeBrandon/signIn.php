7836ab92776ee7a3e06a6b4333e6e89e017fc49e
Brandon Arensmeyer <b73x527@gfcmsu.edu>
2023-02-28 18:51:17 -0700

    change

--- CafeBrandon/signIn.php
+++ CafeBrandon/signIn.php
@@ -8,6 +8,38 @@
     <title>Sign In</title>
 </head>
 <body>
+    <main> 
+        <?php if ($_SERVER['REQUEST_METHOD'] === 'GET') : ?>
+            <?php echo "<h2>Please order anything you would like</h2>"; ?>
+            <form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
+                <div id="fName">
+                    <label id="first" for="catName">First name:</label>
+                    <input id="firstText" type="text" name="catName" />
+                </div>
+                <div id="lName">
+                    <label id="last" for="descr">Last name:</label>
+                    <input id="lastText" type="text" name="descr" />
+                </div>
+                <div>
+                    <label id="password" for="price">Password:</label>
+                    <input id="passwordText" type="text" name="price" />
+                </div>
+                <button id="order" type="submit">Add Item</button>
+            </form>
+        <?php
+        elseif (!in_array("", $_POST)) :
+            // elseif (isset($_POST['catName']) && isset($_POST['descr']) && isset($_POST['price'])):
+            $catName = htmlspecialchars(strip_tags($_POST['catName']));
+            $descr = htmlspecialchars(strip_tags($_POST['descr']));
+            $price = htmlspecialchars(strip_tags($_POST['price']));
+            // echo var_export($_POST['price'], true)."<br>";
+            echo "<h2> Thank you for ordering! <h2>";
+            echo "<h3> You ordered $catName, and added '$descr' to the description<h3> <br>";
+        else :
+            echo "Please fill out all fields<br>";
+        endif;
+        ?>
+    </main>
     
 </body>
 </html>
